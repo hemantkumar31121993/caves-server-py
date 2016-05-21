a4380c4c3a723d044c9b62ebccd126019e6bd9b3
Garvit Pahal <g10pahal@gmail.com>
2016-05-21 12:13:48 +0530

    Improvements

--- server.py
+++ server.py
@@ -1,29 +1,33 @@
 from flask import Flask, request
 import MySQLdb
 import json
+from OpenSSL import SSL
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='game')
 
-conn = MySQLdb.connect(host="localhost", user="root", passwd="wdinlm", db="caves")
+conn = MySQLdb.connect(host="localhost", user="root", passwd="rootp", db="caves")
+conn.autocommit(True)
 cursor = conn.cursor()
 
 def checkauth(req):
     print req
     if (not 'teamname' in req) or (not 'password' in req):
-        return (False, 0)
+        return (False, 0, 0, 0)
     team_name = req['teamname']
     password = req['password']
     cursor.execute("select * from cred where Team = %s and password = %s", (team_name, password))
     if int(cursor.rowcount) > 0:
-  	return (True, int(cursor.fetchone()[2]))
+        result = cursor.fetchone()
+  	return (True, int(result[2]), int(result[3]), int(result[4]))
     else:
-	return (False, 0)
+	return (False, 0, 0, 0)
 
 def levelnchallenge(n, data):
     try:
+        print data
         if n < 8:
             req = json.loads(data)
-	    succ, level = checkauth(req)
+            succ, level, d1, d2 = checkauth(req)
 	    if succ is True and level >= n - 1:
 		cursor.execute("select * from level" + str(n) + "ques where Team = %s", (req['teamname'],))
 		response = {'challenge': cursor.fetchone()[1]}
@@ -34,16 +38,17 @@ def levelnchallenge(n, data):
 	    return json.dumps(response)
         else:
 	    response = {'error': 'Maximum level is 7'}
-            return json.dumps(response)	
-    except:
-	response = {'error': 'Exception occured'}
+            return json.dumps(response)
+    except Exception, e:
+        print e
+	response = {'error': 'There is some problem with the server'}
         return json.dumps(response)
 
 def checkLeveln(n, data):
     try:
 	if n < 8:
             req = json.loads(request.data)
-	    succ, level = checkauth(req)
+	    succ, level, d1, d2 = checkauth(req)
 	    if not 'answer' in req:
 		response = {'error': 'Post data should contain an answer'}
 		return json.dumps(response)
@@ -61,32 +66,34 @@ def checkLeveln(n, data):
 	    return json.dumps(response);
         else:
 	    response = {'error': 'Maximum level is 7'}
-            return json.dumps(response)	
+            return json.dumps(response)
     except:
-	response = {'error': 'Exception occured'}
+	response = {'error': 'There is some problem with the server'}
         return json.dumps(response)
-        
+
 @app.route("/login", methods=['POST'])
 def login():
     try:
         print request.data
         req = json.loads(request.data)
-        succ, level = checkauth(req)
+        succ, level, wf, sf = checkauth(req)
         if succ is True:
-  	    response = {'completedTill': level}
+            response = {'ct': level, 'wf': bool(wf), 'sf': bool(sf)}
         else:
 	    response = {'error': 'Invalid Credentials'}
+        print response
         return json.dumps(response)
-    except:
-	response = {'error': 'Exception occured'}
+    except Exception, e:
+        print e
+	response = {'error': 'There is some problem with the server'}
         return json.dumps(response)
-        
 
-@app.route("/level6challenge", methods=['POST'])
-def level6challenge():
-    
 
-@app.route("/level<int:n>challenge", methods=['POST'])
+#@app.route("/level6challenge", methods=['POST'])
+#def level6challenge():
+
+
+@app.route("/challenge<int:n>", methods=['POST'])
 def getchallenge(n):
     return levelnchallenge(n, request.data)
 
@@ -94,5 +101,46 @@ def getchallenge(n):
 def checkLevel1(n):
     return checkLeveln(n, request.data)
 
+@app.route("/fw", methods=['POST'])
+def foundWand():
+    try:
+        req = json.loads(request.data)
+        succ, level, d1, d2 = checkauth(req)
+        if succ is True:
+            if level == 3:
+                cursor.execute("update cred set WandFound = 1 where Team = %s", (req['teamname'],))
+                response = {'success': True}
+            else:
+                response = {'success': False}
+        else:
+            response = {'error': 'Invalid Credentials'}
+        print response
+        return json.dumps(response);
+    except:
+	response = {'error': 'There is some problem with the server'}
+        return json.dumps(response)
+
+@app.route("/fs", methods=['POST'])
+def freeSpirit():
+    try:
+        req = json.loads(request.data)
+        succ, level, d1, d2 = checkauth(req)
+        if succ is True:
+            if level == 3:
+                cursor.execute("update cred set SpiritFreed = 1 where Team = %s", (req['teamname'],))
+                response = {'success': True}
+            else:
+                response = {'success': False}
+        else:
+            response = {'error': 'Invalid Credentials'}
+        print response
+        return json.dumps(response);
+    except:
+	response = {'error': 'There is some problem with the server'}
+        return json.dumps(response)
+
+#context1 = ('server.crt', 'server.key')
+#app.run(host='0.0.0.0', port=9999, threaded=True, ssl_context=context1)
+
 app.run(host='0.0.0.0', port=9999, threaded=True)
- 
+
--- sqlinit.sql
+++ sqlinit.sql
@@ -4,7 +4,7 @@ use caves;
 
 -- All the passwords are assumed to be MD5 hashes
 
-create table cred (Team varchar(20) not null, Password char(32) not null, currentlevel Integer not null default 0, primary key(Team));
+create table cred (Team varchar(20) not null, Password char(32) not null, currentlevel Integer not null default 0, WandFound Integer not null, SpiritFreed Integer not null,  primary key(Team));
 
 create table level1ques (Team varchar(20) not null, Challenge varchar(50) not null, primary key(Team));
 create table level1sol (Team varchar(20) not null, Password varchar(50) not null, primary key(Team));
@@ -12,7 +12,7 @@ create table level1sol (Team varchar(20) not null, Password varchar(50) not null
 create table level2ques (Team varchar(20) not null, Challenge varchar(256) not null, primary key(Team));
 create table level2sol (Team varchar(20) not null, Password varchar(50) not null, primary key(Team));
 
-create table level3ques (Team varchar(20) not null, Challenge varchar(50) not null, primary key(Team));
+create table level3ques (Team varchar(20) not null, Challenge varchar(350) not null, primary key(Team));
 create table level3sol (Team varchar(20) not null, Password varchar(50) not null, primary key(Team));
 
 create table level4ques (Team varchar(20) not null, Challenge varchar(50) not null, primary key(Team));
@@ -25,15 +25,20 @@ create table level5keys (Team varchar(20) not null, AMat char(112) not null, EVe
 
 create table level6ques (Team varchar(20) not null, Challenge varchar(350) not null, n varchar(350), e varchar(350), primary key(Team));
 create table level6sol (Team varchar(20) not null, Password varchar(50) not null, primary key(Team));
---create table level6keys (Team varchar(20) not null, p varchar(350), q varchar(350), d varchar(350),  primary key(Team));
+#create table level6keys (Team varchar(20) not null, p varchar(350), q varchar(350), d varchar(350),  primary key(Team));
 
 create table level7ques (Team varchar(20) not null, Challenge varchar(250) not null, primary key(Team));
 create table level7sol (Team varchar(20) not null, Password varchar(50) not null, primary key(Team));
 
-insert into cred values ("a", MD5("a"), 0);
+insert into cred values ("a", MD5("a"), 0, 0, 0);
 
 insert into level1ques values ("a", "uhXhQ03nfrv");
 insert into level1sol values ("a", MD5("level1"));
 
 insert into level2ques values ("a", "Lg ccud qh urg tgay ejbwdkt, wmgtf su bgud nkudnk lrd vjfbg. Yrhfm qvd vng sfuuxytj \"vkj_ecwo_ogp_ej_rnfkukf\" wt iq urtuwjm. Ocz iqa jdag vio uzthsivi pqx vkj pgyd encpggt. Uy hopg yjg fhkz arz hkscv ckoq pgfn vu wwygt nkioe zttft djkth.");
-insert into level2sol values ("a", MD5("the_cave_man_may_be_pleased"));
+insert into level2sol values ("a", MD5("level2"));
+
+insert into level3ques values ("a", "Lg ccud qh urg tgay ejbwdkt, wmgtf su bgud nkudnk lrd vjfbg. Yrhfm qvd vng sfuuxytj \"vkj_ecwo_ogp_ej_rnfkukf\" wt iq urtuwjm. Ocz iqa jdag vio uzthsivi pqx vkj pgyd encpggt. Uy hopg yjg fhkz arz hkscv ckoq pgfn vu wwygt nkioe zttft djkth.");
+insert into level3sol values ("a", MD5("level3"));
+
+insert into level4sol values("a", MD5("level4"));
